96c7146f63e6055805e239aa93806b174d8bc0ed
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2022-12-26 01:18:11 +0400

    update

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,49 @@
 	Array.prototype.last = function() {
 		return this[this.length - 1];
 	};
-	// Yandex.Maps
+	// Supported PDF
+	const supportedPdf = function(){
+		var is_pdf = false,
+			plugins = Array.from(window.navigator.plugins || {}),
+			map = plugins.map(function(a){
+				var mp = Array.from(a);
+				if (mp[0].suffixes=='pdf' && !is_pdf){
+					is_pdf = true;
+				}
+				return mp;
+			});
+		return is_pdf;
+	};
+	const isPDF = supportedPdf();
+	// Button Visually Impaired
 	const bvi = new isvek.Bvi({
 		speech: false,
 		panelFixed: false
 	});
+	// Navigation button
+	$(document).on('click', '.nav-btn', function(e){
+		e.preventDefault();
+		$(e.target).closest('.navbar-site').toggleClass('open');
+		return !1;
+	});
+	// Fancybox
+	$.fancybox.defaults.parentEl = ".fancybox__wrapper";
+	$.fancybox.defaults.transitionEffect = "circular";
+	$.fancybox.defaults.transitionDuration = 500;
+	$.fancybox.defaults.lang = "ru";
+	$.fancybox.defaults.i18n.ru = {
+		CLOSE: "Закрыть",
+		NEXT: "Следующий",
+		PREV: "Предыдущий",
+		ERROR: "Запрошенный контент не может быть загружен.<br/>Повторите попытку позже.",
+		PLAY_START: "Начать слайдшоу",
+		PLAY_STOP: "Остановить слайдшоу",
+		FULL_SCREEN: "Полный экран",
+		THUMBS: "Миниатюры",
+		DOWNLOAD: "Скачать",
+		SHARE: "Поделиться",
+		ZOOM: "Увеличить"
+	};
 	$(window).on('resize scroll', function(e){
 		setTimeout(function(){
 			$('.fancybox-content').css({
@@ -14,15 +52,10 @@
 			});
 		}, 5);
 		
-	})
-	$(document).on('click', '.nav-btn', function(e){
-		e.preventDefault();
-		$(e.target).closest('.navbar-site').toggleClass('open');
-		return !1;
 	});
-	$(document).on('click', ".main a", function(e){//[href$='.pdf']
+	$(document).on('click', ".main a", function(e){
 		const base = window.location.origin,
-			reg = new RegExp("^" + base),
+			reg = new RegExp("^" + base, 'gi'),
 			regn = new RegExp('\.pdf$', 'gi'),
 			href = e.target.href;
 		if(reg.test(href)) {
@@ -31,10 +64,11 @@
 					src = url.pathname;
 				if(regn.test(src)){
 					e.preventDefault();
+					let pdf_href = isPDF ? href : base + '/assets/pdfjs/viewer/?file=' + href.replace(reg, '');
 					$.fancybox.open({
 						toolbar: true,
 						type : 'iframe',
-						src: base + '/assets/pdfjs/viewer/?file=' + href.replace(reg, ''),
+						src: pdf_href,
 						opts : {
 							afterShow : function() {
 								$('.fancybox-content').css({
@@ -55,6 +89,7 @@
 			} 
 		}
 	});
+	// Yandex.Maps
 	window.mapInit = function() {
 		const $map = $("#map");
 		if($map.length) {
@@ -115,6 +150,7 @@
 			}
 		}
 	};
+	supportedPdf();
 }(jQuery));
 
 /**
