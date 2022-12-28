3d86f6131e7133f7145f5992869c053eff943b13
ProjectSoft <projectsoft2009@yandex.ru>
2022-12-29 03:17:13 +0400

    update

--- site/assets/templates/projectsoft/develop/plugins/tabenabled/plugin.tabenabled.php
+++ site/assets/templates/projectsoft/develop/plugins/tabenabled/plugin.tabenabled.php
@@ -7,6 +7,14 @@ switch($e->name){
 	case 'OnManagerTopPrerender':
 		$modx->config['global_tabs'] = 1;
 		$out = '<script>modx.config.global_tabs = 1;</script>';
+        $out .= '<style>
+    @media (min-width: 1200px) {
+        #mainMenu #nav #site::before {
+            background: url("/assets/templates/projectsoft/images/logo-navbar-white.png") 0 50% no-repeat;
+            background-size: 9rem;
+        }
+    }
+</style>';
 		$modx->event->output($out);
 		break;
 }
